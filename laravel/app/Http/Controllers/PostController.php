581ffa4552e62145a1fa97ca751ffd1f2b9818a6
suke33 <62455402+suke33@users.noreply.github.com>
2021-03-10 11:07:51 +0900

    ホーム画面フェーズ1の実装

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -6,11 +6,35 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Post;
 use App\Models\Like;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index()
     {
-        //
+        if (Auth::check()) {
+            $user = Auth::user();
+            $posts = Post::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
+            return view('admin.home', compact('user', 'posts'));
+        } else {
+            return view('auth.login');
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        $post = Post::find($request->id);
+        $post->delete();
+        return redirect()->route('post.index');
+    }
+
+    public function create()
+    {
+        return view('admin.democreate');
+    }
+
+    public function show()
+    {
+        return view('admin.demodetail');
     }
 }
