a1889ab1923bf2140dc0f539c2a00bb1bfda1357
nosu8 <curtmqhj@gmail.com>
2021-07-17 22:53:05 +0900

    “scssに変更”

--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -24,7 +24,7 @@ if (mix.inProduction()) {
 }*/
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'resources/sass/style.scss', 'public/css')
     .options({
         postCss: [
             require('postcss-import'),
